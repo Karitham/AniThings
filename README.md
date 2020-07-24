1ff232ebd4a5da9d8a985387dbd907b34c3a1068
Karitham <plouis.pery@gmail.com>
2020-07-24 17:40:15 +0200

    Make the program more user friendly

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RandomAnilistAnime
-Queries a random anime and outputs URL
+
+Is used to query a random anime from anilist and outputs the URL.
+
+You can also simply use a release provided in the release tab.
+
+## Compile yourself
+
+To compile for youself, you need the [go compiler](https://github.com/golang/go)
+
+Then you can just run `go build .` in the repo
--- randomAnime.go
+++ randomAnime.go
@@ -1,29 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/machinebox/graphql"
 )
 
-// RadomAnimeStruct holds data from random anime query
-type RadomAnimeStruct struct {
-	Page struct {
-		Media []struct {
-			SiteURL string `json:"siteUrl"`
-		} `json:"media"`
-	} `json:"Page"`
+func main() {
+	// Introduce the program
+	fmt.Print("Hello, this is a small tool to get random anime from anilist\nPlease enter the number of anime you want to get :\n>")
+
+	// Read event
+	bytes, err := bufio.NewReader(os.Stdin).ReadBytes('\n')
+	if err != nil {
+		fmt.Println("there was an error reading your input", err)
+	}
+
+	// Parse input
+	number, err := strconv.Atoi(strings.TrimSpace(strings.TrimSuffix(strings.TrimSuffix(string(bytes), "\n"), "\r")))
+	if err != nil {
+		fmt.Println("Verify if your input is a number", err)
+	}
+
+	// Run queries
+	for i := 0; i <= number; i++ {
+		time.Sleep(750 * time.Millisecond)
+		go fmt.Println(returnURL())
+	}
+
+	// Don't let the program stop
+	fmt.Print("Press 'Enter' to quit the program ...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
 
-func main() {
-	var res RadomAnimeStruct
+func returnURL() string {
+	var randomAnime struct {
+		Page struct {
+			Media []struct {
+				SiteURL string `json:"siteUrl"`
+			} `json:"media"`
+		} `json:"Page"`
+	}
 
-	// build query
-	graphURL := "https://graphql.anilist.co"
-	client := graphql.NewClient(graphURL)
+	// request
 	req := graphql.NewRequest(`
 	query ($id: Int) {
 		Page(page: $id, perPage: 1) {
@@ -38,10 +64,11 @@ func main() {
 	req.Var("id", rand.New(rand.NewSource(time.Now().UnixNano())).Intn(14626))
 
 	// Run RQ
-	client.Run(context.Background(), req, &res)
+	err := graphql.NewClient("https://graphql.anilist.co").Run(context.Background(), req, &randomAnime)
+	if err != nil {
+		fmt.Println("there was an error while getting random anime :", err)
+	}
 
 	// Print resulting URL
-	fmt.Println(res.Page.Media[0].SiteURL)
-
-	select {}
+	return randomAnime.Page.Media[0].SiteURL
 }
